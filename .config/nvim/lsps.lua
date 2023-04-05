7f487a6e035a81d0e18af2d6602983eaff32f259
Daniil Kachur <kachur.daniil@gmail.com>
2023-04-05 10:14:10 +0400

    Fix some plugins warnings

--- .config/nvim/lsps.lua
+++ .config/nvim/lsps.lua
@@ -32,11 +32,11 @@ cmp.setup({
   })
 })
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+-- local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-require('lspconfig').tsserver.setup {
-  capabilities = capabilities
-}
+-- require('lspconfig').tsserver.setup {
+--   capabilities = capabilities
+-- }
 
 local lsp_installer = require('nvim-lsp-installer')
 
