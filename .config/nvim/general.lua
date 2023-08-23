57b2b43ab682851c6491f9e9d04d332a0f5f0485
Daniil Kachur <kachur.daniil@gmail.com>
2023-08-23 11:05:32 +0400

    Let space and line ending to be empty

--- .config/nvim/general.lua
+++ .config/nvim/general.lua
@@ -1,3 +1,3 @@
 vim.opt.list = true -- enables custom listchars (configured bellow)
-vim.opt.listchars:append "space:⋅" -- show ⋅ instead of space
-vim.opt.listchars:append "eol:↴" -- show ↴ in the end of the line 
+-- vim.opt.listchars:append "space:⋅" -- show ⋅ instead of space
+-- vim.opt.listchars:append "eol:↴" -- show ↴ in the end of the line 
