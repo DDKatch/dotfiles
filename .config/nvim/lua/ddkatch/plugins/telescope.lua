0e4236e8929f37ffe8fbafd0186080c759b1116d
Daniil Kachur <kachur.daniil@gmail.com>
2023-10-24 20:05:56 +0400

    Add autofil for word under the cursor

--- .config/nvim/lua/ddkatch/plugins/telescope.lua
+++ .config/nvim/lua/ddkatch/plugins/telescope.lua
@@ -3,5 +3,6 @@
   vim.keymap.set('n', '<leader>fd', builtin.find_files, {}) -- find files in directory
   vim.keymap.set('n', '<leader>fg', builtin.git_files, {}) -- find files attachet to the git
   vim.keymap.set('n', '<leader>fl', builtin.live_grep, {})
+  vim.keymap.set('n', '<leader>fln', ":Telescope live_grep default_text=<C-r><C-w><CR>", {})
   vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
   vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
