6402d905cd956d9a1c6999cc7ce4a4dce33cff53
Daniil Kachur <kachur.daniil@gmail.com>
2024-02-19 12:27:08 +0400

    Disable for markdown
    Aparently 2+ trailing spaces is a replacement for \n\r in Markdown language

--- .config/nvim/lua/ddkatch/tweaks/automatic-trailing-whitespace.lua
+++ .config/nvim/lua/ddkatch/tweaks/automatic-trailing-whitespace.lua
@@ -1,6 +1,10 @@
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
     pattern = {"*"},
     callback = function()
+      if vim.bo.filetype == "markdown" then
+        return
+      end
+
       local save_cursor = vim.fn.getpos(".")
       pcall(function() vim.cmd [[%s/\s\+$//e]] end)
       vim.fn.setpos(".", save_cursor)
