9b88b45f7da43ec3d069640584c8342f4ce2ddfc
Daniil Kachur <kachur.daniil@gmail.com>
2024-06-20 13:35:16 +0400

    Search hignlighted word by leader-flh command

--- .config/nvim/lua/ddkatch/plugins/telescope.lua
+++ .config/nvim/lua/ddkatch/plugins/telescope.lua
@@ -1,9 +1,12 @@
 local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<leader>fd', builtin.find_files, {}) -- find files in directory
-vim.keymap.set('n', '<leader>fg', builtin.git_files, {}) -- find files attachet to the git
+vim.keymap.set('n', '<leader>fg', builtin.git_files, {})  -- find files attachet to the git
 vim.keymap.set('n', '<leader>fl', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fln', ":Telescope live_grep default_text=<C-r><C-w><CR>", {})
+vim.keymap.set('n', '<leader>flh', ":Telescope live_grep default_text=<C-r><C-w><CR>", {})
+-- vim.keymap.set('n', '<leader>flh', function()
+--   vim.cmd("Telescope live_grep default_pattern='" .. vim.fn.getreg('/') .. "'")
+-- end, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
