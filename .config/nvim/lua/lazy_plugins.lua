6d1cc83d3231db03785f45280842e321afe60659
Daniil Kachur <kachur.daniil@gmail.com>
2025-04-04 14:23:10 +0400

    Fix alias - now it works

--- .config/nvim/lua/lazy_plugins.lua
+++ .config/nvim/lua/lazy_plugins.lua
@@ -235,13 +235,13 @@ return {
     opts = {
       -- add any opts here
       -- for example
-      provider = "openai",
+      provider = "claude",
       openai = {
-        endpoint = "https://api.openai.com/v1",
-        model = "gpt-4o",  -- your desired model (or use gpt-4o, etc.)
-        timeout = 30000,   -- Timeout in milliseconds, increase this for reasoning models
+        endpoint = "https://api.anthropic.com/v1/messages",
+        model = "claude-3-7-sonnet-20250219", -- your desired model (or use gpt-4o, etc.)
+        timeout = 30000,                      -- Timeout in milliseconds, increase this for reasoning models
         temperature = 0,
-        max_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+        max_tokens = 8192,                    -- Increase this to include reasoning tokens (for reasoning models)
         --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
       },
     },
