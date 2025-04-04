7865cb859215a7ab6450f438f71db656c4175672
Daniil Kachur <kachur.daniil@gmail.com>
2025-04-04 13:30:02 +0400

    Add trouble plugin hotkeys explicitly

--- .config/nvim/lua/lazy_plugins.lua
+++ .config/nvim/lua/lazy_plugins.lua
@@ -159,7 +159,41 @@ return {
   {
     "folke/trouble.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
+    keys = {
+      {
+        "<space>xx",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)",
+      },
+      {
+        "<space>xX",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics (Trouble)",
+      },
+      {
+        "<space>cs",
+        "<cmd>Trouble symbols toggle focus=false<cr>",
+        desc = "Symbols (Trouble)",
+      },
+      {
+        "<space>cl",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "LSP Definitions / references / ... (Trouble)",
+      },
+      {
+        "<space>xL",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Location List (Trouble)",
+      },
+      {
+        "<space>xQ",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Quickfix List (Trouble)",
+      },
+    },
+
     opts = {
+
       -- your configuration comes here
       -- or leave it empty to use the default settings
       -- refer to the configuration section below
